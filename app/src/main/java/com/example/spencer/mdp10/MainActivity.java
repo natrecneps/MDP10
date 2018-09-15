f3df543f5e818d602907962f2240660ac3f00c77
Spencer <spencertan@hotmail.sg>
2018-09-15 21:35:48 +0800

    working now

--- app/src/main/java/com/example/spencer/mdp10/MainActivity.java
+++ app/src/main/java/com/example/spencer/mdp10/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     TextView incomingMessages;
     StringBuilder messages;
 
-    private static final UUID MY_UUID_INSECURE = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final UUID MY_UUID_INSECURE = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
 
     BluetoothDevice mBTDevice;
 
